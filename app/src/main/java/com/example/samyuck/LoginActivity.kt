f947e4a9eb99fce1dbae3da15dd7e378bdf2098f
seong su kim <know.warehouse02@gmail.com>
2025-04-06 22:57:15 +0900

    회원가입 / 로그인 세부 화면 구현

--- app/src/main/java/com/example/samyuck/LoginActivity.kt
+++ app/src/main/java/com/example/samyuck/LoginActivity.kt
@@ -1,27 +1,23 @@
-package com.example.samyuck;
+package com.example.samyuck
 
-import android.content.Intent;
-import android.app.Activity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 
-public class LoginActivity extends Activity {
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login); // 로그인 화면 XML과 연결
+    fun onSignUpClick(view: View) {
+        val intent = Intent(this, SignUpActivity::class.java)
+        startActivity(intent)
+    }
 
-        // 버튼 연결
-        Button guestButton = findViewById(R.id.guestButton);
-        guestButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        });
+    fun onLoginClick(view: View) {
+        val intent = Intent(this, InputLogActivity::class.java)
+        startActivity(intent)
     }
 }
