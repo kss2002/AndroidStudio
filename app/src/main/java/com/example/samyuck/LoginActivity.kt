593f18b1ec95833f7170dcc94dc69908be6a8b6b
seong su kim <know.warehouse02@gmail.com>
2025-04-25 09:49:31 +0900

    로그인 / 회원가입 살짝 수정 / 게스트로 이동 에러 고침

--- app/src/main/java/com/example/samyuck/LoginActivity.kt
+++ app/src/main/java/com/example/samyuck/LoginActivity.kt
@@ -9,6 +9,12 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        // 게스트 버튼 클릭 리스너 설정
+        findViewById<View>(R.id.guestButton).setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 
     fun onSignUpClick(view: View) {
